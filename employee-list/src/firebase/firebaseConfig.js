d09c951a2b58ce6549e176accb8f6beeaed993df
saiful Islam <saifulmeraz897@gmail.com>
2022-02-02 20:42:40 +0600

    creating project

---
+++ employee-list/src/firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBbucWZNWDO0QtXOVTXc_sGXJG5Jwh_lP8",
+    authDomain: "employee-list-aa43f.firebaseapp.com",
+    projectId: "employee-list-aa43f",
+    storageBucket: "employee-list-aa43f.appspot.com",
+    messagingSenderId: "718301697398",
+    appId: "1:718301697398:web:fa4d409b6c80f218379657"
+}
